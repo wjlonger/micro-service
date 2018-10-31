d659014fa046d42e8e59af0b7fe8c04531ec0712
735626035@qq.com <735626035@qq.com>
2018-10-31 14:07:39 +0800

    SpringCloud 架构设计

--- route/src/main/java/com/coder/route/RouteApplication.java
+++ route/src/main/java/com/coder/route/RouteApplication.java
@@ -2,10 +2,14 @@ package com.coder.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class RouteApplication {
 
     public static void main(String[] args) {

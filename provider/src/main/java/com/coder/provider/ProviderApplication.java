abf4ed39108443e91af15315374f4db6db78e7a7
wujunlong <wujunlong@mankebao.com>
2019-02-18 11:31:41 +0800

    集成ElasticSearch

--- provider/src/main/java/com/coder/provider/ProviderApplication.java
+++ provider/src/main/java/com/coder/provider/ProviderApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.coder.provider.mapper")
 @EnableTransactionManagement
 @EnableEurekaClient
 @SpringBootApplication
-
+//这里basePackages对应你dao所在包
+@EnableElasticsearchRepositories(basePackages = "com.coder.provider.repository")
 public class ProviderApplication {
 
     public static void main(String[] args) {
